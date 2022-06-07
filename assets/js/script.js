f953bc2c3fbfc44889568afd9899758aa08c2ca0
Owen Newlove <owen.newlove@gmail.com>
2022-06-07 17:26:07 -0400

    added Id's to all the rows corresponding to their military times. Then created a function to display the colors for the past, present, and future by using a for loop that references an array variable that was also created starting with 0 and ending at hour 23. If statements were used within the for loop to add classes based on the timeBlocksArr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 
 
 
+var timeBlocksArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
 var currentHour = moment().hour()
 
 var currentDay = $("#currentDay")
@@ -8,3 +10,27 @@ console.log(currentHour)
 
 var systemTime = moment().format("dddd, MMMM Do YYYY")
 currentDay.text(systemTime)
+
+
+function displayTimeBlockColors() {
+
+    for (var i = 0; i < timeBlocksArr.length; i++) {
+        var currentTextEl = $("#" + timeBlocksArr[i])
+
+        if (currentHour > timeBlocksArr[i]) {
+
+            currentTextEl.addClass("past")
+
+        }
+        else if (currentHour === timeBlocksArr[i]) {
+
+            currentTextEl.addClass("present")
+        }
+        else {
+            currentTextEl.addClass("future")
+        }
+    }
+
+}
+
+displayTimeBlockColors()
